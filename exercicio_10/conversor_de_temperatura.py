c9697dc5d280799b80725e000c9293258168a8bf
Gabriele Oliveira <oliveiragabrielesouza@gmail.com>
2021-03-24 16:04:35 -0300

    exercicio 10

---
+++ exercicio_10/conversor_de_temperatura.py
@@ -0,0 +1,3 @@
+celsius = float(input('Digite a temperatura em grau celsius:'))
+resultado = (celsius / 5) * 9 + 32
+print ('A temperatura de {}°C corresponde a {:.1f}°F'.format(celsius, resultado))
