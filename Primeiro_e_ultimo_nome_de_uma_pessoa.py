ec8bec02bea112ca85aad93c8811c1a08b77a183
OliveiraGabriele <oliveiragabrielesouza@gmail.com>
2021-07-27 18:28:38 -0300

    Criando um programa que faça o computador pensar em um numero inteiro entre 0 e 5 e peça para o ucuário tentar descobrir qual foi o número escolhido pelo computador. O programa deverá escrever na tela se o usuário acertou ou errou.

---
+++ Primeiro_e_ultimo_nome_de_uma_pessoa.py
@@ -0,0 +1,4 @@
+nome = str(input('Digite seu nome comleto:').strip())
+primeiro, ultimo = nome.split(), nome.split()
+print('Primeiro nome: {}'.format(primeiro[0]),('\nUltimo nome: {}'.format(ultimo[len(ultimo)-1])))
+
