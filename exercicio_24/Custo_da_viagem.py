304f3282e87e62e36472a041201524a67eb5b187
OliveiraGabriele <oliveiragabrielesouza@gmail.com>
2021-07-28 12:01:36 -0300

    Criando um programa que leia um ano qualquer e mostre se ele é bissexto.

---
+++ exercicio_24/Custo_da_viagem.py
@@ -0,0 +1,10 @@
+
+distancia = float(input('Qual a distancia percorrida em km?'))
+a = 0.50 * distancia
+b = 0.45 * distancia
+if distancia <= 200:
+    print('A passagem custa:R${:.2f}'.format(a))
+else:
+    distancia > 200
+    print('A passagens custa:R${:.2f}'.format(b))
+
