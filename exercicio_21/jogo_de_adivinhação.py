e53c648079a22b193f5eb1e06f3c00444d3437b4
Gabriele Oliveira <oliveiragabrielesouza@gmail.com>
2021-07-28 11:19:04 -0300

    Update jogo_de_adivinhação.py

---
+++ exercicio_21/jogo_de_adivinhação.py
@@ -0,0 +1,7 @@
+from random import randint
+maquina = randint(0, 5)
+usuario = int(input('Em que número eu pensei?:'))
+if usuario == maquina:
+    print("Você conseguiu adivinhar!")
+else:
+    print('Infelismente você não conseguiu adivinhar, pois pensei no numero {} não no {}.'.format(maquina, usuario))  
